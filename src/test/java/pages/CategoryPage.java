d44911a4e6af56bb0ebaf0966b97a648602e6f81
Renganathan <renganathan@Renganathans-MacBook-Pro.local>
2021-03-01 16:55:08 +0530

    The files are addes

---
+++ src/test/java/pages/CategoryPage.java
@@ -0,0 +1,35 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import utils.BasePage;
+
+import static utils.Driver.driver;
+import static utils.PropertyReader.prop;
+
+
+public class CategoryPage {
+    BasePage b=new BasePage();
+    public void find_element()
+    {
+        WebElement webElement= driver.findElementById("blibli.mobile.commerce:id/tv_search_count");
+        Assert.assertTrue(webElement.getText().contains(prop.getProperty("product")));
+    }
+    public void navigate()
+    {
+        b.clickByText(prop.getProperty("Category"), 10);
+    }
+
+    public void second_navigate()
+    {
+        b.clickByText(prop.getProperty("Text_name"),10);
+        b.clickByText(prop.getProperty("product"),10 );
+        b.clickByText(prop.getProperty("Variable"),10);
+    }
+
+    public void scrollclick()
+    {
+        b.scrollToElementWithText(prop.getProperty( "endtext"),10);
+        b.clickByText(prop.getProperty("endtext"),10);
+    }
+}
